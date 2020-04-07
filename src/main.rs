4fab4e96cc962e5f2b35c9a8dbf44372d632cd49
Sam Smith <13868670+SamHSmith@users.noreply.github.com>
2020-04-07 23:38:10 +0200

    safety

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,62 @@
-use std::ffi::{CStr, CString};
-use libc::c_char;
 
-#[link(kind = "dylib", name = "gtkwin")]
-extern "C" {
+mod native{
+    use std::ffi::{CString};
+    use libc::c_char;
 
-    fn gtk_set_title(title: &CStr);
+    #[link(kind = "dylib", name = "gtkwin")]
+    extern "C" {
 
-    fn gtk_set_size(width: i32, height: i32);
+        fn gtk_set_title(title: *const c_char);
 
-    fn is_close_button_pressed() -> i32;
+        fn gtk_set_size(width: i32, height: i32);
 
-    fn gtk_refresh_window();
+        fn is_close_button_pressed() -> i32;
 
-    fn gtk_show_window();
+        fn gtk_refresh_window();
 
-    fn gtk_hide_window();
+        fn gtk_show_window();
 
-    fn gtk_create_window();
+        fn gtk_hide_window();
 
-    fn gtk_create_window_and_show(width: i32, height: i32, title: *const c_char);
+        fn gtk_create_window();
 
-    fn gtk_init_gtk();
+        fn gtk_create_window_and_show(width: i32, height: i32, title: *const c_char);
+
+        fn gtk_init_gtk();
+    }
+
+    pub fn window_should_close() -> bool{
+        unsafe { is_close_button_pressed() != 0 }
+    }
+
+    //resets should close
+    pub fn window_refresh(){
+        unsafe { gtk_refresh_window(); }
+    }
+
+
+
+    pub fn create_window_and_show(width: i32, height: i32, title: &str){
+        let cstr=CString::new(title).unwrap();
+        unsafe { gtk_create_window_and_show(width, height, cstr.as_ptr()); }
+    }
+
+    pub fn window_init(){ unsafe {gtk_init_gtk();} }
 }
+
+use native::*;
 
 fn main() {
     unsafe {
-        gtk_init_gtk();
-        gtk_create_window_and_show(
+        window_init();
+        create_window_and_show(
             640,
             480,
-            CString::new("Look this is rust").unwrap().as_c_str().as_ptr(),
+            "Haha, this window is \"safe\"",
         );
 
-        while(is_close_button_pressed()==0){
-            gtk_refresh_window();
+        while !window_should_close() {
+            window_refresh();
         }
     }
     println!("Hello, world!");
